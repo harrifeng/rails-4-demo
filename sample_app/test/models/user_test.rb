ce08097a58659acb3d4aa5f9c7253738de64fd12
Haoran Feng <haoran.feng@alo7.com>
2016-12-08 10:35:23 +0800

    page 293

--- sample_app/test/models/user_test.rb
+++ sample_app/test/models/user_test.rb
@@ -37,4 +37,10 @@ class UserTest < ActiveSupport::TestCase
       assert_not @user.valid?, "#{invalid_address} should be invalid"
     end
   end
+
+  test "email addresses should be unique" do
+    duplicate_user = @user.dup
+    @user.save
+    assert_not duplicate_user.valid?
+  end
 end
