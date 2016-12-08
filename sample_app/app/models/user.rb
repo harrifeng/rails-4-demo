0d94a3751dbcf078ddb981dc036bd0b17e2faad1
Haoran Feng <haoran.feng@alo7.com>
2016-12-08 17:56:05 +0800

    We save the password_digest, not the password (and the password is not accessible at all, it is handled by rails) So we had to save the remember_digest, not the remember_token, but we can see the remember_token by making it as attr_accessor, so that self.remember_token is readable and writable
    On page 432

--- sample_app/app/models/user.rb
+++ sample_app/app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
+  attr_accessor :remember_token
   before_save { self.email = email.downcase }
   validates :name, presence: true, length: { maximum: 50 }
 
@@ -16,5 +17,14 @@ class User < ActiveRecord::Base
              BCrypt::Engine::MIN_COST :
              BCrypt::Engine.cost
     BCrypt::Password.create(string, cost: cost)
+  end
+
+  def self.new_token
+    SecureRandom.urlsafe_base64
+  end
+
+  def remember
+    self.remember_token = self.new_token
+    update_attribute(:remember_digest, User.digest(remember_token))
   end
 end
