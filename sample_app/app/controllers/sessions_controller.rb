9c145f436de287b76b3fcfe2e67a969115fe610f
Haoran Feng <haoran.feng@alo7.com>
2016-12-08 14:56:30 +0800

    page 399

--- sample_app/app/controllers/sessions_controller.rb
+++ sample_app/app/controllers/sessions_controller.rb
@@ -3,6 +3,11 @@ class SessionsController < ApplicationController
   end
 
   def create
-    render 'new'
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+    else
+      flash[:danger] = 'Invalid email/password combination'
+      render 'new'
+    end
   end
 end
