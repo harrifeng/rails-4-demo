97d6d92f82b5d44ec920cdf7cba5450d454aa95e
Haoran Feng <haoran.feng@alo7.com>
2016-12-08 10:41:05 +0800

    bundle exec rake db:migrate AFTER close rails c sandbox

--- sample_app/db/schema.rb
+++ sample_app/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161208013743) do
+ActiveRecord::Schema.define(version: 20161208023719) do
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
