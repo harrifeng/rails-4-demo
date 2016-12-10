0ae36db2c1d454be46a8a4e6f0b5d3a044973b25
Haoran Feng <haoran.feng@alo7.com>
2016-12-10 16:24:01 +0800

    page 608

--- sample_app/test/integration/password_resets_test.rb
+++ sample_app/test/integration/password_resets_test.rb
@@ -1,7 +1,29 @@
 require 'test_helper'
 
 class PasswordResetsTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    ActionMailer::Base.deliveries.clear
+    @user = users(:michael)
+  end
+
+  test "password resets" do
+    get new_password_reset_path
+    assert_template 'password_resets/new'
+    # Invalid email
+    post password_resets_path, password_reset: { email: "" }
+    assert_not flash.empty?
+    assert_template 'password_resets/new'
+
+    # Valid email
+    post password_resets_path, password_reset: { email: @user.email }
+    assert_not_equal @user.reset_digest, @user.reload.reset_digest
+    assert_equal 1, ActionMailer::Base.deliveries.size
+    assert_not flash.empty?
+    assert_redirected_to root_url
+    # Password rest form
+    user = assigns(:user)
+    # Wrong email
+    get edit_password_reset_path(user.reset_token, email: "")
+    assert_redirected_to root_url
+  end
 end
